045a29d5a13b6f1887975bc7cc1f86f4a688437d
Bob Strecansky <bob.strecansky@gmail.com>
2016-12-01 20:45:53 -0500

    Updating installer to use local packages

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt-get -y install zsh build-essential cmake
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-curl -o $HOME/.zshrc https://raw.githubusercontent.com/bobstrecansky/dotfiles/master/.zshrc
-curl -o $HOME/.vimrc https://raw.githubusercontent.com/bobstrecansky/dotfiles/master/.vimrc
+bash pyenv/pyenv-instaler 
+curl -o ~/.zshrc https://raw.githubusercontent.com/bobstrecansky/dotfiles/master/.zshrc
+curl -o ~/.vimrc https://raw.githubusercontent.com/bobstrecansky/dotfiles/master/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p $HOME/.vim/colors/
 curl -o $HOME/.vim/colors/ http://bitbucket.org/sjl/badwolf/raw/tip/colors/badwolf.vim
